c946d6eb9b38639edba201f7dc210087010aea9b
Binod <binod@Binitas-MacBook-Air.local>
2018-12-12 20:50:10 -0700

    Commit 5 - Getting to Know You answered

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ for(let i=0; i < 201; i +=2){
 }
 
 
+//3.
 let catTalk = "Love me, pet me! HSSSSSSS!";
 for(let i = 0; i < 20; i++){
     console.log(catTalk);
 }
 
 
+//4.
 for(let i = 0; i < 101; i++){
     if(i % 3 === 0 && i % 5 === 0){
         console.log("FizzBuzz");
@@ -30,3 +32,27 @@ for(let i = 0; i < 101; i++){
         console.log(i);
     }
 }
+
+//5. getting to know you
+const shahzad = ["Shahzad", 1000, "Austin"];
+const jim = ["Jim", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const daniel = ["Daniel", 186, "Dallas"];
+const ryan = ["Ryan", 65, "Denver"];
+//1.
+shahzad[0] = "gameboy";
+//2.
+daniel[1] = daniel[1] + 1;
+//3.
+ryan[2] = "Gotham City";
+//4.
+reuben[2] = "Chicago";
+//5.
+jim[2] = "Denver, Ny, Paris";
+
+
+
+
+
+
+
