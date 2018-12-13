4bc5868beb7b8a64b825ff0910fd01435ec56bfc
Binod <binod@Binitas-MacBook-Air.local>
2018-12-12 20:03:07 -0700

    commit 3 - Excited Kittens answered

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ for(let i = 1; i < 21; i++){
 for(let i=0; i < 201; i +=2){
     console.log(i);
 }
+
+
+let catTalk = "Love me, pet me! HSSSSSSS!";
+for(let i = 0; i < 20; i++){
+    console.log(catTalk);
+}
+
+
